ac3d5b1c1ffbef54594400db073efbfa3117f490
ericgu2017 <49860926+ericgu2017@users.noreply.github.com>
2019-06-21 13:33:29 +0800

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -49,7 +49,7 @@ if [ ! -f "./dotnet-sdk-2.2.203-linux-x64.tar.gz" ]; then
 fi
 #download 0secretroom
 sudo mkdir /root/0secretroom -p
-wget https://github.com/ericgu2017/0secretroom/releases/download/0.2/latest.tar.gz -O latest.tar.gz
+wget https://github.com/ericgu2017/0secretroom/releases/download/0.3/latest.tar.gz -O latest.tar.gz
 sudo tar zxvf latest.tar.gz -C /root/0secretroom
 sudo cp /root/0secretroom/web.service /etc/systemd/system
 sudo systemctl enable web.service
